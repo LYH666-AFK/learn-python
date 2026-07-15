447a72179101cf1a9ac5e7036b10cfc296784b8e
LYH666-AFK <1736401845@qq.com>
2026-07-15 15:13:10 +0800

    课后习题--字符串作业对答案

--- 练习.py
+++ 练习.py
@@ -1,9 +1,13 @@
 #格式化练习
+
+# 定义需要的变量
 name="传智博客"
-stock_price="19.99"
+stock_price=19.99
 stock_code="003032"
+# 股票 价格 每日 增长 因子
 stock_price_daily_growth_factor=1.2
-growth_days="7"
-result=19.99*1.2**7
+growth_days=7
+
+finally_stock_price=stock_price*stock_price_daily_growth_factor**growth_days
 print(f"公司：{name}，股票代码:{stock_code}，当前股价:{stock_price}")
-print("每日增长系数是:%.1f，经过%s天的增长后，股价达到了:%.2f"%(stock_price_daily_growth_factor,growth_days,result))
+print("每日增长系数是:%.1f，经过%d天的增长后，股价达到了:%.2f"%(stock_price_daily_growth_factor,growth_days,finally_stock_price))
