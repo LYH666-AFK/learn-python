de7a0c4995e32370fb1de48b9d6d1f79786cc9a5
LYH666-AFK <1736401845@qq.com>
2026-07-20 22:04:33 +0800

    判断语句嵌套学习

--- 第三章/判断语句综合案例练习.py
+++ 第三章/判断语句综合案例练习.py
@@ -1,3 +1,4 @@
+#自己手打的复杂版（思路一）
 import random
 num=random.randint(1,10)
 
@@ -43,3 +44,34 @@ elif guess_number1<num:
         print("恭喜你第二次猜对了")
 else:
     print("恭喜你第一次就猜对了")
+
+
+#答案的简单版（思路二）
+import random
+num=random.randint(1,10)
+
+guess_number1=int(input("输入你要猜测的数字："))
+if guess_number1==num:
+    print("恭喜，第一次就猜中了")
+else:
+    if guess_number1>num:
+        print("你猜测的数字大了")
+    else:
+        print("你猜测的数字小了")
+
+    guess_number2=int(input("再次输入你要猜测的数字："))
+
+    if guess_number2==num:
+        print("恭喜，第二次猜中了")
+    else:
+        if guess_number2>num:
+            print("你猜测的数字大了")
+        else:
+            print("你猜测的数字小了")
+
+        guess_number3=int(input("第三次输入你要猜测的数字："))
+
+        if guess_number3==num:
+            print("第三次猜中了")
+        else:
+            print("三次机会用完了，没有猜中。")
