66726ea6d94650a1a023e6d7221e9c3b745ea5d1
LYH666-AFK <1736401845@qq.com>
2026-08-05 22:03:02 +0800

    list列表的语法和索引

---
+++ 06第六章--数据容器/list列表的语法和索引.py
@@ -0,0 +1,31 @@
+"""
+演示数据容器：list
+"""
+#定义一个列表 list
+my_list=["LYH","math","python"]
+print(my_list)
+print(type(my_list))
+
+my_list=["LYH",666,True]
+print(my_list)
+print(type(my_list))
+
+#定义一个嵌套的列表
+my_list=[[1,2,3],[4,5,6]]
+print(my_list)
+print(type(my_list))
+
+#通过下标索引取出对应位置的数据(注意：不能超出范围)
+my_list=["Tom","Lily","Rose"]
+#列表[下标索引],从前向后从0开始，每次+1， 从后向前从-1开始，每次-1
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+#倒序取出
+print(my_list[-1])
+print(my_list[-2])
+print(my_list[-3])
+
+#取出嵌套列表的元素
+my_list=[[1,2,3],[4,5,6]]
+print(my_list[1][1])
